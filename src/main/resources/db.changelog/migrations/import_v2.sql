b3f5cb2190b703b55e7a17709d50b633569f30ce
Vladimir Razuvaev <skyudaff@mail.ru>
2023-12-20 21:27:08 +0300

    added liquibase

---
+++ src/main/resources/db.changelog/migrations/import_v2.sql
@@ -0,0 +1,5 @@
+-- changeset skyudaff:2
+INSERT INTO netology.persons(name, surname, age, phone_number, city_of_living)
+VALUES ('Ivan', 'Ivanov', 28, '5550055', 'Moscow'),
+       ('Peter', 'Petrov', 20, '7771111', 'SPB'),
+       ('Sidor', 'Sidorov', 30, '7771100', 'Moscow');
